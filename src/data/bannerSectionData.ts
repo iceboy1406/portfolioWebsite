7cef99c5357c6edf19d535e3fd63d616fca9027f
Eko Susilo <ekosusilo140604@gmail.com>
2024-07-02 11:12:43 +0700

    refactor: change cv url

--- src/data/bannerSectionData.ts
+++ src/data/bannerSectionData.ts
@@ -8,7 +8,7 @@ const bannerSectionData = {
     description: `I am passionate Frontend Developer. I love to build modern and futuristic website with high performance and SEO friendly. I have 8 months experience work as Frontend Developer at Nearven Inovasi Digital. I also like playing codewars to improve my programming logic skill. And lastly I also like working with Typescript because it's improve a lot Developer Experience and prevent bug from the Application.`,
     downloadCVButton: {
         value: 'Download CV',
-        href: 'https://drive.google.com/file/d/1MzgsCUHTRCAy-ktPH5BOFJG7IMpTQNVR/view?usp=sharing',
+        href: 'https://drive.google.com/file/d/15uQfGxGUHorqKyzF2MGrTocRdAkzsAow/view?usp=sharing',
     },
     exploreMoreButton: {
         value: 'Explore More',
